1da7aca441d453bce49529c4fd54710d931e5b51
leokhachatorians <leokhachatorians@gmail.com>
2016-10-16 17:25:02 -0700

    Orgainzed

--- tictactoe.py
+++ tictactoe.py
@@ -3,9 +3,9 @@ import random
 class TicTacToe():
     def __init__(self):
         self.board = [
-                ' ', ' ', ' ',
-                ' ', ' ', ' ',
-                ' ', ' ', ' '
+                '.', '.', '.',
+                '.', '.', '.',
+                '.', '.', '.'
         ]
         self.combinations = (
             (0,1,2), (3,4,5), (6,7,8),
@@ -20,7 +20,7 @@ class TicTacToe():
     def open_spots(self):
         open_spots = []
         for c, i in enumerate(self.board):
-            if i == ' ':
+            if i == '.':
                 open_spots.append(c)
         return open_spots
 
@@ -35,9 +35,22 @@ class TicTacToe():
         return marked
 
     def potentials(self, player):
+        """
+        Return a list of all open spots and player marked spots
+        to determine exactly what spots can be used to win the game.
+        """
         return self.open_spots() + self.grab_player_marked(player)
 
     def who_won(self):
+        """
+        Iterate over the game winning combinations and determine
+        if the marks made by the player match up with any of
+        the combinations.
+
+        If a player has been found to have won, return the player.
+        Else, return none.
+        """
+
         for player in ('x', 'o'):
             player_marks = self.grab_player_marked(player)
             for combination in self.combinations:
@@ -50,13 +63,19 @@ class TicTacToe():
         return None
 
     def finished(self):
-        if ' ' not in self.board:
-            return True
-        if self.who_won() != None:
+        """
+        If there are no more empty spots or if 'who_won()'
+        returns something other than None, the game is over.
+        """
+        if '.' not in self.board or self.who_won() != None:
             return True
         return False
 
     def check_if_tied(self):
+        """
+        If the game is both finished and 'who_won()' returns
+        None, then the game is tied.
+        """
         if self.finished() and self.who_won() == None:
             return True
         return False
@@ -64,24 +83,24 @@ class TicTacToe():
     def who(self, player):
         return 'o' if player == 'x' else 'x'
 
-    def prune(self, branch, player, alpha, beta):
-        if branch.finished():
-            if branch.who_won() == 'o':
-                return 1
-            elif branch.who_won() == 'x':
-                return -1
+    def prune(self, depth, player, alpha, beta):
+        if depth.finished():
+            if depth.who_won() == 'o':
+                return 100
+            elif depth.who_won() == 'x':
+                return -100
             else:
                 return 0
         for move in self.open_spots():
-            branch.mark_spot(player, move)
-            score = self.prune(branch, self.who(player), alpha, beta)
-            branch.mark_spot(' ', move)
+            depth.mark_spot(player, move)
+            score = self.prune(depth, self.who(player), alpha, beta)
+            depth.mark_spot('.', move)
             if player == 'o':
                 if score > alpha:
                     alpha = score
                 if alpha >= beta:
                     return beta
-            else:
+            elif player == 'x':
                 if score < beta:
                     beta = score
                 if beta <= alpha:
@@ -91,21 +110,19 @@ class TicTacToe():
         else:
             return beta
 
-def plan(board, player):
-    a = -2
-    paths = []
-    if len(board.open_spots()) == 9:
-        return 4
-    for move in board.open_spots():
-        board.mark_spot(player, move)
-        score = board.prune(board, board.who(player), -2, 2)
-        board.mark_spot(' ', move)
-        if score > a:
-            a = score
-            paths = [move]
-        elif score == a:
-            paths.append(move)
-    return random.choice(paths)
+    def plan(self, board, player):
+        a = -100
+        paths = []
+        for move in board.open_spots():
+            board.mark_spot(player, move)
+            score = board.prune(board, board.who(player), -100, 100)
+            board.mark_spot('.', move)
+            if score > a:
+                a = score
+                paths = [move]
+            elif score == a:
+                paths.append(move)
+        return paths[random.randint(0, len(paths))]
 
 if __name__ == '__main__':
     t = TicTacToe()
@@ -125,7 +142,7 @@ if __name__ == '__main__':
             break
 
         comp = t.who(player)
-        comp_move = plan(t, comp)
+        comp_move = t.plan(t, comp)
         t.mark_spot(comp, comp_move)
         t.print_board()
     print("Winner: {}".format(t.who_won()))
